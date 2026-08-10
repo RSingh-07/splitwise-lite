e8720958a31bed1a781095f36c060c80176d3ff3
riyaindzone <riya.indzone@gmail.com>
2026-08-10 11:49:28 +0530

    Add cascade delete from Group to Expense to prevent FK constraint errors

--- src/main/java/com/example/splitwise_lite/entity/Group.java
+++ src/main/java/com/example/splitwise_lite/entity/Group.java
@@ -1,6 +1,8 @@
 package com.example.splitwise_lite.entity;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "expense_groups")
 public class Group{
@@ -8,6 +10,8 @@ public class Group{
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Expense> expense;
 
     public Group(){}
     public Group(String name){this.name = name;}
