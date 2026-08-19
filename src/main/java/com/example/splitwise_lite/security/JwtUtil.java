b58fa0ac7651e6d85fb979b17a464bb2ae285a26
riyaindzone <riya.indzone@gmail.com>
2026-08-19 12:18:28 +0530

    Add JwtUtil with token generation and signing key logic

---
+++ src/main/java/com/example/splitwise_lite/security/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.example.splitwise_lite.security;
+
+
+import com.example.splitwise_lite.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    private Key getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+        return key;
+    }
+
+    public String generateToken(User user){
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+}
