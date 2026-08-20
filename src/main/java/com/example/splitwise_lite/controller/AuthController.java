28429894859136f2fff0333ee266caba16f32d2c
riyaindzone <riya.indzone@gmail.com>
2026-08-20 12:42:31 +0530

    Add login endpoint to AuthController with JWT generation

--- src/main/java/com/example/splitwise_lite/controller/AuthController.java
+++ src/main/java/com/example/splitwise_lite/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.example.splitwise_lite.controller;
 
+import com.example.splitwise_lite.dto.LoginRequest;
 import com.example.splitwise_lite.dto.SignupRequest;
 import com.example.splitwise_lite.entity.Role;
 import com.example.splitwise_lite.entity.User;
 import com.example.splitwise_lite.repository.UserRepository;
+import com.example.splitwise_lite.security.JwtUtil;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +19,12 @@ public class AuthController {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/auth/register")
@@ -31,6 +37,17 @@ public class AuthController {
        userRepository.save(user);
        return user;
 
+    }
+
+
+    @PostMapping("/auth/login")
+    public String login(@Valid @RequestBody LoginRequest loginRequest){
+        User userName = userRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("Email or password is incorrect!!"));
+        if(!passwordEncoder.matches(loginRequest.getPassword(), userName.getPassword())){
+            throw new UsernameNotFoundException("Email or password is incorrect!");
+        }
+        return jwtUtil.generateToken(userName);
     }
 
 
